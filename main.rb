4f288ef2af7cc7b1891033d3e614bb18b4a8f067
leont <leon.traykov@gmail.com>
2022-10-21 19:42:46 -0400

    gem colorized installed

--- main.rb
+++ main.rb
@@ -1,8 +1,10 @@
 require_relative 'lib/console_interface'
 require_relative 'lib/game'
+require 'colorize'
+require 'colorized_string'
 
 # Приветсвие
-puts "Всем привет!"
+puts "Вас приветствует консольная игра на выживание!".light_green
 
 # Загружаем случайное слово из файла
 word = File.readlines(__dir__ + '/data/words.txt', encoding: 'UTF-8', chomp: true).sample
